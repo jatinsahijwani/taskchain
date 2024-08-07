ee06bbb3c794bde47e435225df8f5d75ab4e1fdb
Anirudh Singh Chouhan <140189477+AnirudhSingh07@users.noreply.github.com>
2024-08-07 15:38:35 +0530

    pages

---
+++ app/userdash/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import  Userdashboard  from '@/components/component/userdashboard'
+const page = () => {
+  return (
+    <div>
+      <Userdashboard/>
+    </div>
+  )
+}
+
+export default page
